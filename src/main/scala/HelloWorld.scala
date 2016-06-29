a2ebc4f01a7ab99cc36c166041db600621c3bdaf
justyna <zielonydomek@gmail.com>
2016-06-29 23:42:52 +0200

    Add spark

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,8 +1,23 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
 /**
   * Created by bucio on 29.06.16.
   */
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val conf = new SparkConf().setAppName("appName").setMaster("local")
+    val sc = new SparkContext(conf)
+    var NUM_SAMPLES = 1000;
+    val count = sc.parallelize(1 to NUM_SAMPLES).map{i =>
+      val x = Math.random()
+      val y = Math.random()
+      if (x*x + y*y < 1) 1 else 0
+    }.reduce(_ + _)
+    println("Pi is roughly " + 4.0 * count / NUM_SAMPLES)
+
+
   }
 }
