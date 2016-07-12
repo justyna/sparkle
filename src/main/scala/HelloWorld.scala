cc121b4e94b96a7f7369a85c79b7858f3cdf1732
justyna <zielonydomek@gmail.com>
2016-07-12 18:55:15 +0200

    Read Json WIP

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -10,14 +10,17 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("appName").setMaster("local")
     val sc = new SparkContext(conf)
-    var NUM_SAMPLES = 1000;
+    /*var NUM_SAMPLES = 1000;
     val count = sc.parallelize(1 to NUM_SAMPLES).map{i =>
       val x = Math.random()
       val y = Math.random()
       if (x*x + y*y < 1) 1 else 0
     }.reduce(_ + _)
-    println("Pi is roughly " + 4.0 * count / NUM_SAMPLES)
+    println("Pi is roughly " + 4.0 * count / NUM_SAMPLES)*/
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
+    val df = sqlContext.read.format("json").load("src/main/resources/repositories.json")
+    df.printSchema()
 
   }
 }
