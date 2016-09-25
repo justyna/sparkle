b5d848ca23b1f2922137a708efc09dcbf1aafa19
Justyna <zielonydomek@gmail.com>
2016-09-25 20:51:24 +0200

    migrate zakup to zakupy, add coalsce, remove output director, save to parquet

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,3 +1,5 @@
+import java.io.File
+
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
@@ -5,14 +7,23 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.explode
 import com.databricks.spark.avro._
 
+
 object App {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("appName").setMaster("local")
-    //System.setProperty("hadoop.home.dir", "C:\\hadoop")
+    val conf = new SparkConf()
+      .setAppName("appName")
+      .setMaster("local")
+    System.setProperty("hadoop.home.dir", "C:\\hadoop")
     val sc = new SparkContext(conf)
+    sc.hadoopConfiguration.set("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
+    sc.hadoopConfiguration.set("parquet.enable.summary-metadata", "false")
+
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     import sqlContext.implicits._
 
+    import org.apache.commons.io.FileUtils
+    import org.apache.commons.io.filefilter.WildcardFileFilter
+    FileUtils.deleteDirectory(new File("src/main/resources/output/"))
     val df = sqlContext.read.format("json").load("src/main/resources/")
     df.registerTempTable("df")
     val purchase = df.select($"id", $"firstName", $"lastName",  explode($"purchase")).as("purchase")
@@ -24,12 +35,13 @@ object App {
     betterPurchase.filter(betterPurchase("price")> 20).map(p => "First name: " + p(1) + " last name: " + p(2) + "name of product: " + p.getAs[String]("name") + " price: "+ p.getAs[String]("price")).collect().foreach(println)
     betterPurchase.filter(betterPurchase("price")> 20).map(p => "Product: " + p.getAs[String]("name") + " price: "+ p.getAs[String]("price")).distinct().collect().foreach(println)
     betterPurchase.groupBy("id").sum("price").show()
-
-    betterPurchase.write.avro("src/main/resources/output/avro")
-    betterPurchase.write.json("src/main/resources/output/json")
-    betterPurchase.write
+    //coalesce for set one partition
+    betterPurchase.coalesce(1).write.avro("src/main/resources/output/avro" )
+    betterPurchase.coalesce(1).write.json("src/main/resources/output/json")
+    betterPurchase.coalesce(1).write
       .format("com.databricks.spark.csv")
       .option("header", "true")
       .save("src/main/resources/output/csv/")
+    betterPurchase.coalesce(1).write.parquet("src/main/resources/output/parquet")
   }
 }
